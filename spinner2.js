de4c91e185093914a4a15a2881563d36cfd23533
Bushra Patel <bpatel60@yahoo.com>
2024-04-14 17:42:36 -0400

    Refactoring spinner by eliminating the repetetive setTimeout statements to clean up the code

--- spinner2.js
+++ spinner2.js
@@ -1,16 +1,13 @@
-const spinnerAnimation = function(chars) {
-  let delay = 100;
+const chars = ["|", "/", "-", "\\", "|", "/", "-", "\\", "|"];
+let delay = 100;
 
-  for (let char of chars) {
-    setTimeout(() => {
-      process.stdout.write(`\r${char}  `);
-    }, delay);
-    delay += 200;
-  };
-
+for (let char of chars) {
   setTimeout(() => {
-    process.stdout.write("\n");
+    process.stdout.write(`\r${char}  `);
   }, delay);
+  delay += 200;
 };
 
-spinnerAnimation(["|", "/", "-", "\\", "|", "/", "-", "\\", "|"]);
+setTimeout(() => {
+  process.stdout.write("\n");
+}, delay);
